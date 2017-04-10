727936232bb6e85494be8616475a835de32596b1
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-04-09 22:08:54 -0300

    Trying to change workspace

--- startup.go
+++ startup.go
@@ -7,6 +7,7 @@ func startup() {
 
 	screen := screens[0]
 	doWorkspaceNew(screen.workspace, nil)
+	doWorkspaceMigrate(screen.workspace, nil)
 
 	doExec(nil, []string{"spotify"})
 	doExec(nil, []string{"pavucontrol"})
