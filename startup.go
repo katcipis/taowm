d4dda3327f5be0aaa97ddb6ef439f68be4f6f54a
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-04-09 22:40:49 -0300

    One last try

--- startup.go
+++ startup.go
@@ -4,9 +4,10 @@ package main
 func startup() {
 	doExec(nil, []string{"kitty"})
 	doExec(nil, []string{"kitty"})
-	//screen := screens[0]
-	//doWorkspaceNew(screen.workspace, nil)
-	//doWindow(screen.workspace, nil)
+
+	screen := screens[0]
+	doWorkspace(screen.workspace, next)
+
 	doExec(nil, []string{"spotify"})
 	doExec(nil, []string{"pavucontrol"})
 	doExec(nil, []string{"google-chrome-stable"})
