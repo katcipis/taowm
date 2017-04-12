0f6f2eb9e95d3424db1beb9a6ba267bff487141f
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-04-12 17:32:44 -0300

    Only start slower/annoying part

--- startup.go
+++ startup.go
@@ -2,12 +2,6 @@ package main
 
 // commands executed at startup
 func startup() {
-	doExec(nil, []string{"kitty"})
-	doExec(nil, []string{"kitty"})
-
-	screen := screens[0]
-	doWorkspace(screen.workspace, next)
-
 	doExec(nil, []string{"spotify"})
 	doExec(nil, []string{"pavucontrol"})
 	doExec(nil, []string{"google-chrome-stable"})
